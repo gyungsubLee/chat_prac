61b5a310e4507a9b7b4c9dff4acf1b9eb40aec22
gyoungsubLee <dltjq2323@gmail.com>
2025-04-29 23:21:04 +0900

    Feat: chat - 순수 Websocket

---
+++ src/main/java/com/example/chat/chat/config/SimpleWebsocketHandler.java
@@ -0,0 +1,44 @@
+package com.example.chat.chat.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Slf4j
+@Component
+public class SimpleWebsocketHandler extends TextWebSocketHandler {
+
+    // 연결 관리: Thread Safe 한 Set 사용
+    private final Set<WebSocketSession> sessions = ConcurrentHashMap.newKeySet();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+        log.debug("Connected : {}", session.getId());
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String payload = message.getPayload();
+        log.debug("received message : {}", payload);
+
+        for (WebSocketSession s : sessions) {
+            if (s.isOpen()) {
+                s.sendMessage(new TextMessage(payload));
+            }
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
+        log.debug("disconnected : {}", session.getId());
+    }
+
+}
---
+++ src/main/java/com/example/chat/chat/config/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package com.example.chat.chat.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final SimpleWebsocketHandler simpleWebsocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        // '/connect' url로 websocket 연결이 들어오면 인자로 넘기는 handler 클래스가 처리한다.
+        registry.addHandler(simpleWebsocketHandler, "/connect")
+                // securityconfig에서의 cors 예외는 http 요청에 대한 예외이다
+                // 따라서 websocket 프로토콜에 대한 요청은 대해서는 별로의 cors 설정이 필요하다.
+                .setAllowedOrigins("http://localhost:3000");   // 클라이언트 url cors 허용
+    }
+}
