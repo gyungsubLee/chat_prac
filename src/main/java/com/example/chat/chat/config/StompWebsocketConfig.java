33085ef1ae3636378de10219db3722545965db12
gyoungsubLee <dltjq2323@gmail.com>
2025-05-27 21:13:47 +0900

    Feat: chat - STOMP 커넥션 인증 handler

--- src/main/java/com/example/chat/chat/config/StompWebsocketConfig.java
+++ src/main/java/com/example/chat/chat/config/StompWebsocketConfig.java
@@ -1,5 +1,6 @@
 package com.example.chat.chat.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -7,12 +8,16 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import java.util.List;
+
 
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class StompWebsocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private static final String CLIENT_URI="http://localhost:3000";
+    private final StompHandler stompHandler;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -31,9 +36,10 @@ public class StompWebsocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.enableSimpleBroker("/topic");
     }
 
+    // 웹소캣 요청(connect, subscribe, disconnect 등)의 요청 시에는 http header 등 http 메세지를 넣어올 수 있고, 이를 interceptor를 통해 가로채 토큰 등으로 인증 처리 할 수 있다.
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors();
+        registration.interceptors(stompHandler);
     }
 
 }
